d3cdfdab79454756660e72326a20c4b31559e063
Arthur Monteiro <amonteirom96@gmail.com>
2019-10-25 18:06:28 +0000

    Emanoel

--- Repositories/Bairro.php
+++ Repositories/Bairro.php
@@ -4,8 +4,13 @@ namespace App\Repositories;
 use App\Model\Neighborhood;
 
 class Bairro extends BaseRepository{
-	
+
 	public function __construct(){
 		$this->model =  new Neighborhood;
 	}
+
+    public function Emanoel()
+    {
+
+    }
 }
